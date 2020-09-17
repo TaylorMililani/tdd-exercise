0e904e63459fcaacae205676919082ad42eae033
Taylor Tompkins <taylormililani@gmail.com>
2020-09-16 21:10:05 -0700

    Tests have passed

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,37 @@
 # blackjack_score.rb
-
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand)
 
+  hand.each do |card|
+    if !VALID_CARDS.include?(card)
+      raise ArgumentError.new("#{card} is not a valid card")
+    end
+  end
+
+  hand.sort_by! { |card| card == 'Ace' ? 1 : 0 }
+
+  score = 0
+  i = 0
+  hand.each do |card|
+    if card == 'Jack' || card == 'Queen' || card == 'King'
+      score += 10
+    elsif card == 'Ace'
+      if score > 10
+        score += 1
+      else
+        score += 11
+      end
+    else
+      score += card
+    end
+
+  end
+
+  if score > 21
+    raise ArgumentError.new("Score cannot be more than 21")
+  end
+
+  return score
 end
+
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -18,26 +18,73 @@ describe 'Blackjack Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
-
+    expect(score).must_equal 7
   end
 
   it 'facecards have values calculated correctly' do
+
+    hand = ['Ace', 'Jack', 7]
+
+    score = blackjack_score(hand)
+
+    expect(score).must_equal 18
 
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
 
+    hand = [4, 3, 3, 'Ace']
+
+    score = blackjack_score(hand)
+
+    expect(score).must_equal 21
+
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+
+    hand = ['King', 9 , 'Ace']
+
+    score = blackjack_score(hand)
+
+    expect(score).must_equal 20
 
   end
 
   it 'raises an ArgumentError for invalid cards' do
 
+    hand = [9, 17, 'Jack']
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = ['Nine', 7, 5]
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+    hand = ['Jack', 5, -2]
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
   end
 
   it 'raises an ArgumentError for scores over 21' do
+
+    hand = ['Jack', 10, 2]
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = ['Jack', 5, 9]
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = ['Jack', 5, 7]
+    expect {
+      blackjack_score(hand)
+    }.must_raise ArgumentError
 
   end
 end
